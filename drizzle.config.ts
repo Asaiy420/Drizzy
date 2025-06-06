9aad9162e73f07d7b0fa7c63fa44314efefd746f
Asaiy420 <kcaryan05@gmail.com>
2025-06-06 19:53:37 +0545

    add: drizzle config

---
+++ drizzle.config.ts
@@ -0,0 +1,17 @@
+import * as dotenv from "dotenv";
+import { defineConfig } from "drizzle-kit";
+
+dotenv.config({ path: ".env" });
+
+if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not set");
+}
+
+export default defineConfig({
+  out: "./drizzle",
+  schema: "./lib/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
