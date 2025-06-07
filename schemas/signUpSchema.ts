525c17cc0fa00e5cddf0b632b36244772f64fcf3
Asaiy420 <kcaryan05@gmail.com>
2025-06-07 13:08:35 +0545

    correct mispelled word

--- schemas/signUpSchema.ts
+++ schemas/signUpSchema.ts
@@ -3,9 +3,9 @@ import {z} from "zod";
 export const signUpSchema = z.object({
     email: z.string().email({message: "Please enter a valid email"}),
     password: z.string().min(6, {message: "Password must be at least 6 characters"}),
-    confirmPasswod: z.string().min(6, {message: "Password must be at least 6 characters"}),
+    confirmPassword: z.string().min(6, {message: "Password must be at least 6 characters"}),
 })
-.refine((data) => data.password === data.confirmPasswod, {
+.refine((data) => data.password === data.confirmPassword, {
     message: "Passwords do not match",
-    path: ["confirmPasswod"],
+    path: ["confirmPassword"],
 })
